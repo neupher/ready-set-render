3b8a06647ee8d7d3d28ffd7aa9fea1cdf477d377
neupher <tapaniheikkinennz@gmail.com>
2024-11-30 19:59:51 +0000

    Add workspace, make viewport handle resizing

--- main.js
+++ main.js
@@ -19,3 +19,10 @@ gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 // Clear the canvas
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Allow for resizing
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+});
